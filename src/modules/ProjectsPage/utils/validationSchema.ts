578e8ebdc3757d59d2b2289a7137af226bcfa912
SeddiMK <web.egorovm@gmail.com>
2025-01-06 14:22:05 +0300

    fix: clear

--- src/modules/ProjectsPage/utils/validationSchema.ts
+++ src/modules/ProjectsPage/utils/validationSchema.ts
@@ -1,13 +1,10 @@
-import { z } from 'zod'
+import { z } from 'zod';
 
 export const projectsFilterFormSchema = z.object({
    projectName: z.string().optional(),
-   taskId: z.number({ message: 'Номер задачи должен быть числом' })
+   taskId: z
+      .number({ message: 'Номер задачи должен быть числом' })
       .min(1, { message: 'Номер задачи не может быть отрицательным числом' })
       .optional()
-      .or(z.literal(''))
-      // .transform(e => e === "" ? undefined : e)
-})
-
-
-
+      .or(z.literal('')),
+});
