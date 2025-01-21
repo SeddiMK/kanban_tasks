e57d890b0735d919c7c6503fd4aaf00a3671e8df
SeddiMK <web.egorovm@gmail.com>
2025-01-21 12:22:53 +0300

    fix: clear code

--- src/pages/project/[project-slug].tsx
+++ src/pages/project/[project-slug].tsx
@@ -1,5 +1,4 @@
 import { KanbanPage } from '@/modules/KanbanPage/KanbanPage';
-import { useRouter } from 'next/router';
 import NavLayout from '@/modules/ProjectsPage/components/layout';
 
 export default function ProjectRoute() {
